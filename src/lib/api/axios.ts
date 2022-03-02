5088a87ba06e68d3f0e0084e8b4fea2e5dc1a496
juyohan <dygks8556@naver.com>
2022-03-02 21:00:32 +0900

    서버로부터 에러 응답이 왔을 때, 처리

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import { ACCESS_TOKEN, API_BASE_URL } from '../constants';
 import { getCookie } from '../../util/CookieHandler';
+import { ToastHandle } from '../../util/ToastHandle';
 
 type Method = 'get' | 'post';
 
 const axiosFetch = async <T>(method: Method, url: string, params?: any): Promise<ApiRes<T>> => {
   const token: string = getCookie(ACCESS_TOKEN);
+  const { addToast } = ToastHandle();
+
   let res;
   if (token) {
     res = await axios({
@@ -15,12 +18,24 @@ const axiosFetch = async <T>(method: Method, url: string, params?: any): Promise
       headers: {
         Authorization: 'Bearer ' + token
       }
+    }).catch(error => {
+      const errorRes = error.response;
+      addToast({
+        message: errorRes.data.message
+      });
+      return errorRes;
     });
   } else {
     res = await axios({
       method: method,
       url: API_BASE_URL + url,
       params: params
+    }).catch(error => {
+      const errorRes = error.response;
+      addToast({
+        message: errorRes.data.message
+      });
+      return errorRes;
     });
   }
   return res.data;
